57572c153e06cdc5a74d7ed6576487e621e9d02b
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-05-27 21:38:11 -0400

    Defined computerSelection, trouble with correct game outcome

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // Prompt player with game information
 // Prompt player with question "What is your hand choice?" and store into playerSelection
 // Make function computerPlay which will have computer randomly returns rock, paper, scissor
-// Store computerPlay into ComputerSelection
+// Store computerPlay into computerSelection
 // If player inputs rock: computer inputs scissors, player wins | computer inputs paper, computer wins | else draw
 // If player inputs scissors: computer inputs paper, player wins | computer inputs rock, computer wins | else draw
 // If player inputs paper: computer inputs rock, player wins | computer inputs scissors, computer wins | else draw
@@ -16,49 +16,48 @@
 alert("Lets play a game of Rock, Paper, Scissors");
 
 // Prompt player with question of hand choice and store into playerSelection
-let playerSelection = prompt("Choose rock, paper or scissors");
-
+let playerSelection = prompt("Choose rock, paper or scissors").toLowerCase();
+console.log(playerSelection);
 // Make function computerPlay which will have computer randomly returns array of strings [rock, paper, scissor] then use math.floor of array length and math.random
 // Store randomized answer into computerSelection
-function computerPlay() {
-    const arr = ["Rock", "Paper", "Scissors"];
-    const computerSelection = arr[Math.floor(Math.random() * arr.length)];
-    return computerSelection;
+function computerPlay(computerSelection) {
+    let arr = ["rock", "paper", "scissors"];
+    return computerSelection = arr[Math.floor(Math.random() * arr.length)];
 }
 // Test function to see if computer is returning expected output
 console.log(computerPlay());
-
-// If player and computer input same choice == try again/draw
+// If player and computer input same choice === try again/draw
 // If player inputs rock: computer inputs scissors, player wins | computer inputs paper, computer wins | else draw
 // If player inputs scissors: computer inputs paper, player wins | computer inputs rock, computer wins | else draw
 // If player inputs paper: computer inputs rock, player wins | computer inputs scissors, computer wins | else draw
 // Return string of outcome
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        alert("Computer has also chose" + computerSelection + ". It is a tie! Play again.");
+function playRound(playerSelection, computerSelection) { 
+    computerSelection = computerPlay();
+    if (playerSelection === computerSelection) {
+        alert("Computer also chose " + computerSelection + ". It is a tie! Play again.");
     } 
     else if (playerSelection = "rock") {
         if (computerSelection = "scissors") {
-            alert("Player won");
+            alert("Computer chose " + computerSelection + ". You win!");
         }
         else {
-            alert("Computer won");
+            alert("Computer chose " + computerSelection + ". You lose!");
         }
     }
     else if (playerSelection = "scissors") {
         if (computerSelection = "paper") {
-            alert("Player won");
+            alert("Computer chose " + computerSelection + ". You win!");
         }
         else {
-            alert("Computer won");
+            alert("Computer chose " + computerSelection + ". You lose!");
         }
     }
     else if (playerSelection = "paper") {
         if (computerSelection = "rock") {
-            alert("Player won");
+            alert("Computer chose " + computerSelection + ". You win!");
         }
         else {
-            alert("Computer won");
+            alert("Computer chose " + computerSelection + ". You lose!");
         }
     }
 }
